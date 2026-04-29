d0d8138718e99917b5bb7216ee9570bc9f82486d
Braxton Gingher <zuppies123@gmail.com>
2026-04-29 19:44:58 -0400

    Should have fixed the column from auto resizing itself

--- src/main/java/com/bibleapp/pages/MemorizationPage.java
+++ src/main/java/com/bibleapp/pages/MemorizationPage.java
@@ -217,9 +217,6 @@ public class MemorizationPage extends VBox {
         HBox.setHgrow(leftColumn, Priority.NEVER);
 
         rightColumn.setMaxWidth(Double.MAX_VALUE);
-        /*rightColumn.prefWidthProperty().bind(columnsContainer.widthProperty()
-                .subtract(leftColumn.widthProperty())
-                .subtract(columnsContainer.spacingProperty()));*/
 
         columnsContainer.getChildren().addAll(leftColumn, rightColumn);
         VBox.setVgrow(columnsContainer, Priority.ALWAYS);
