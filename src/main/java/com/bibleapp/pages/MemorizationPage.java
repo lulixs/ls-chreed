03a0294387c4c38a84a738d75ac0007d85d9d30e
Chumnuo <cm1655@messiah.edu>
2026-04-29 21:47:10 -0400

    Revert "Merge pull request #29 from lulixs/MemorizationColSizeBug"
    This reverts commit 31c05df6aab1b64fc2c56bb54a0ed7c34ceb7792, reversing
    changes made to 2b463f6fab19d23a2c6b0eb0fa168f8f447e5326.

--- src/main/java/com/bibleapp/pages/MemorizationPage.java
+++ src/main/java/com/bibleapp/pages/MemorizationPage.java
@@ -215,8 +215,9 @@ public class MemorizationPage extends VBox {
 
         HBox.setHgrow(rightColumn, Priority.ALWAYS);
         HBox.setHgrow(leftColumn, Priority.NEVER);
-
-        rightColumn.setMaxWidth(Double.MAX_VALUE);
+        rightColumn.prefWidthProperty().bind(columnsContainer.widthProperty()
+                .subtract(leftColumn.widthProperty())
+                .subtract(columnsContainer.spacingProperty()));
 
         columnsContainer.getChildren().addAll(leftColumn, rightColumn);
         VBox.setVgrow(columnsContainer, Priority.ALWAYS);
